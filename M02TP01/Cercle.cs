ebeeed12bb3aecb01b2a00de4494e48eb2774b38
Yann GABORIT <yann.gaborit2023@campus-eni.fr>
2024-01-02 15:19:02 +0100

    add gitignore Version 1

---
+++ M02TP01/Cercle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02TP01
+{
+    public class Cercle : Forme
+    {
+        public Cercle()
+        {
+            int Rayon();
+        }
+    }
+}
---
+++ M02TP01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace M02TP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var formes = new List<Forme> {
+            new Cercle { Rayon = 3 },
+            new Rectangle { Longueur = 4, Largeur = 3 },
+            new Carre { Longueur = 3 },
+            new Triangle { A = 4, B = 5, C = 6 }
+             };
+            foreach (Forme forme in formes)
+            {
+                Console.WriteLine(forme);
+            }
+
+        }
+    }
+}
+    
+    
