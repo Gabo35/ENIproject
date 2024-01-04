679fddb09d344654c8f051c5384f3e70554a6f8b
Yann GABORIT <yann.gaborit2023@campus-eni.fr>
2024-01-04 14:28:59 +0100

    Version original

---
+++ Module5Razor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module5Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
